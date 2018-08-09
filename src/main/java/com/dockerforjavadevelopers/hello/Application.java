5e72e06efac57dad74ee7b23786ba64b7b06bdbc
shalimalli <42216250@users.noreply.github.com>
2018-08-09 15:04:59 +0530

    test

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -1,20 +1,21 @@
 package com.dockerforjavadevelopers.hello;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-    }
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+	}
 
 }
--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -1,15 +1,14 @@
 package com.dockerforjavadevelopers.hello;
 
-
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World\n";
-    }
-    
+
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World\n";
+	}
+
 }
