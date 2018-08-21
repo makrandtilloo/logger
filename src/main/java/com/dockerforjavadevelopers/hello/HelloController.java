4f0026d995890f1abef47d15443ab0c1d5275b92
makrandtilloo <42530748+makrandtilloo@users.noreply.github.com>
2018-08-21 17:07:10 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -3,21 +3,26 @@ package com.dockerforjavadevelopers.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 @RestController
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String index() {
-		return "Hello World\n";
+		
+		private static final Log LOGGER = LogFactory.getLog(ExampleController.class);
+
+	@RequestMapping("/log")
+	public String log() {
+		String message = "This line was written to the log.";
+		String secondMessage = "This line was also written to the log with the same Trace ID.";
+		LOGGER.info(message);
+		LOGGER.info(secondMessage);
+		return message;
 	}
+		
 	
-	@RequestMapping("/hi")
-	public String hi() {
 		
-		
-		
-		
-		return "reached hi\n";
-	}
+	
 
 }
